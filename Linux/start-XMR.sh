6a322a591b367d2c610cc17806048a2d24953592
@youngmartin45 <nurfaisalyusuf@gmail.com>
2021-03-03 17:02:36 +0700

    apa

--- Linux/start-XMR.sh
+++ Linux/start-XMR.sh
@@ -2,8 +2,9 @@
 
 
 chmod +x xmrig
+randomName=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
 
-./xmrig -o rx-asia.unmineable.com:3333 -u XMR:83oRwH1fxVYCSy9cCr1KTKDkDUThmXmgDL8xVFxkJUaScFPUSuFZpQdTejc3nNxt8LiBfvV7hJcV5Xh3spSEFQPjMczMEgh.YM45-Worker -k -a rx/0
+./xmrig -o rx-asia.unmineable.com:3333 -u XMR:83oRwH1fxVYCSy9cCr1KTKDkDUThmXmgDL8xVFxkJUaScFPUSuFZpQdTejc3nNxt8LiBfvV7hJcV5Xh3spSEFQPjMczMEgh.YM45-$randomName -k -a rx/0
 
 
 done
