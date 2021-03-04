6c8b199efef2c94c729244a9e5f1d3ba59619773
@youngmartin45 <nurfaisalyusuf@gmail.com>
2021-03-04 22:04:25 +0700

    apa

--- Linux/start-BNB.sh
+++ Linux/start-BNB.sh
@@ -2,8 +2,9 @@
 
 
 chmod +x xmrig
+randomName=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
 
-./xmrig -o rx-asia.unmineable.com:3333 -u BNB:bnb1udq3s2yn2q7mq5mlpkrj98uj58s7dujmjrj7nw:13223.YM45-Worker -k -a rx/0
+./xmrig -o rx-asia.unmineable.com:3333 -u BNB:bnb1udq3s2yn2q7mq5mlpkrj98uj58s7dujmjrj7nw:13223.YM45-$randomName#x4ib-eax8 -k -a rx/0 --cpu-max-threads-hint 100
 
 
 done
